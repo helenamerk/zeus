fc13d4b099d6c4c31fa73f1c5531fe07f6a15d4a
Yannis Spiliopoulos <yannisspiliopoulos@asana.com>
2019-05-18 07:31:02 -0700

    Helper methods for vehicle

--- zeus/models/vehicle.py
+++ zeus/models/vehicle.py
@@ -1,6 +1,10 @@
-from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, Float, String, DateTime, ForeignKey
 
 from zeus import db
+from zeus.utils.smartcar import (
+    client, AccessTokens, maybe_get_fresh_access_tokens,
+    get_battery, get_charge
+)
 
 class Vehicle(db.Model):
 
@@ -20,7 +24,40 @@ class Vehicle(db.Model):
 
     # Live updating
     percent_remaining = Column(Float)
-    charge_status = Column(String)
+    range = Column(Float)
+    charge_state = Column(Integer)
+    is_plugged_in = Column(Boolean)
+
 
     # Settings
     desired_range = Column(Integer)
+
+    def is_queued(self):
+        return self.spot is not null and not self.is_plugged_in
+
+    def is_charging(self):
+        return self.spot is not null and self.is_plugged_in
+
+    def access_tokens(self):
+        return AccessTokens(
+            token=self.access_token,
+            expiration=self.access_expiration,
+            refresh=self.refresh_token
+        )
+
+    def refresh(self):
+        access_tokens = maybe_get_fresh_access_tokens(self.access_tokens())
+        battery = get_battery(self.id, access_tokens.token)
+        charge = get_charge(self.id, access_tokens.token)
+
+        self.access_token = access_tokens.token
+        self.access_expiration = access_tokens.expiration
+        self.refresh_token = access_tokens.refresh
+
+        self.percent_remaining = battery.percent_remaining
+        self.range = battery.range
+        self.chage_state = charge.state.value
+        self.is_plugged_in = charge.is_plugged_in
+        db.session.add(self)
+        db.session.commit()
+        return self
