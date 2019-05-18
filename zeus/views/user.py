f5f9a1f1817c7244f406afe8f88ad31b611add8b
Yannis Spiliopoulos <yannisspiliopoulos@asana.com>
2019-05-18 02:10:37 -0700

    user creation

--- zeus/views/user.py
+++ zeus/views/user.py
@@ -1,5 +1,7 @@
-from flask import render_template
-from zeus import app
+from datetime import datetime
+from flask import render_template, request, redirect, url_for
+from zeus import app, db
+from zeus.models.user import User
 
 @app.route("/user/login", methods=['GET'])
 def login_page():
@@ -17,8 +19,14 @@ def signup_page():
     return render_template("user_signup.html")
 
 @app.route("/user", methods=['POST'])
-def create_user(user_id):
-    pass
+def create_user():
+    data = request.get_json(force=True)
+    data["today_departure"] = datetime.fromtimestamp(data["today_departure"])
+    data["default_departure"] = datetime.fromtimestamp(data["default_departure"])
+    new_user = User(**data)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for("user_page", user_id=new_user.id))
 
 @app.route("/user/<int:user_id>", methods=['GET'])
 def user_page(user_id):
