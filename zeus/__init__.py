7ff2213c6a8ce3ca80c93f939db67657f4914d1c
Sanketh Katta <sanketh@smartcar.com>
2019-05-18 01:17:48 -0700

    Turns out flask already has a default route for static file serving.

--- zeus/__init__.py
+++ zeus/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('./static', filename)
-
 #This is a weird circular dependency but the docs told me to do it :/
 #http://flask.pocoo.org/docs/1.0/patterns/packages/#simple-packages
 import zeus.views
