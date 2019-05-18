ae762763e17007c53bc5909254c962e4bbc84438
Helena Merk <helena.lmtm@gmail.com>
2019-05-18 04:23:17 -0700

    sudo valet

--- zeus/views/valet.py
+++ zeus/views/valet.py
@@ -14,10 +14,31 @@ def valet_login_verify():
 
 @app.route("/valet/dashboard", methods=['GET'])
 def valet_page():
-    return render_template("valet.html")
+    # TODO: 
+    # get all ev spots
+    # while spot == empty, ==> valet_next_car()
+    #
+    # elseif status >= threshold, make unlock button available
+    # 
+    # proceed to flow (unplug, unlock, move, lock) ==> leaves us with a vacant spot
+    #
+    # valet_next_car()
+
+
+    ### 
+    # Valet dashboard should contain actions on top, and data on the bottom. 
+    # Data: stats about the day, ie #s of vehs in each state
+    # Actions: 
+        # Button(s) to move vehicle out of charging spot
+        # Button to start valet_next_car() process
+    ###
+
+    return render_template("valet_dashboard.html", vehicles=[{'spot': '123'}, {'spot': '222'}])
 
 @app.route("/valet/next", methods=['GET'])
 def valet_next_car():
+    # calculate next car in queue
+    # => proceed to flow (unlock, move, plug, lock)
     """
     Calculate and return the next vehicle swap to the client
     """
