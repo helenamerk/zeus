71919ce17e9bce018fc479665e8b1418eb07bd68
Sanketh Katta <sanketh@smartcar.com>
2019-05-18 00:58:05 -0700

    Create routes for user and valet pages.

--- zeus/views/user.py
+++ zeus/views/user.py
@@ -1,7 +1,29 @@
 from flask import render_template
 from zeus import app
 
-@app.route("/user/<int:user_id>")
-def show_user_page(user_id):
+@app.route("/user/login", methods=['GET'])
+def login_page():
+    return render_template("user_login.html")
+
+@app.route("/user/login", methods=['POST'])
+def login_verify():
+    """
+    Verify the user's login credentials
+    """
+    pass
+
+@app.route("/user/signup", methods=['GET'])
+def signup_page():
+    return render_template("user_signup.html")
+
+@app.route("/user", methods=['POST'])
+def create_user(user_id):
+    pass
+
+@app.route("/user/<int:user_id>", methods=['GET'])
+def user_page(user_id):
     return render_template("user.html", user_id=user_id)
 
+@app.route("/user/<int:user_id>", methods=['PUT'])
+def update_user(user_id):
+    pass
