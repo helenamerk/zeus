7a510eef145a538cbf6f05f71c59a0a727f81492
Yannis Spiliopoulos <yannisspiliopoulos@asana.com>
2019-05-18 03:55:25 -0700

    Fix misspelling

--- zeus/lib/smartcar.py
+++ zeus/lib/smartcar.py
@@ -2,7 +2,7 @@ import smartcar
 
 from zeus.lib.util import memoized
 
-@memoize
+@memoized
 def client():
     CLIENT_ID = '52e8d23b-b296-4f8b-89a0-18b64fac4b38'
     CLIENT_SECRET = '13a5b398-ebf1-433f-a82f-60a5224548d8'
